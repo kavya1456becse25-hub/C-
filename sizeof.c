d5c72e31b329d3029e67e0a1c7e5e8b55f6a4ee7
kavya1456becse25-hub <kavya1456.becse25@chitkara.edu.in>
2026-01-31 20:16:30 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,60 @@
+// #include <stdio.h>
+// int main(){
+//     int size_of_int=sizeof(int);
+//     int size_of_float=sizeof(float);
+//     int size_of_char=sizeof(char);
+//     int size_of_double=sizeof(double);
+//     int size_of_long=sizeof(long);
+//     printf("size of int data type:%d\n",size_of_int);
+//     printf("size of float data type:%d\n",size_of_float);
+//     printf("size of char data type:%d\n",size_of_char);
+//     printf("size of double data type :%d\n",size_of_double);
+//     printf("size of long data type:%d\n",size_of_long);
+//     return 0;
+// }
+
+// int main(){
+//     const int intVal=10;
+//     const float floatVal=10.234;
+//     const char charVal='a';
+//     const double doubleVal=2.122335454587;
+//     printf("integer literal:%d\n",intVal);
+//     printf("float literal:%f\n",floatVal);
+//     printf("char literal:%d\n",charVal);
+//     printf("double literal:%.2f\n",doubleVal);
+// }
+
+// int main(){
+//     const char str[]="Welcome\nTo\nChitkara University";
+//     printf("%s",str);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     const char str[]="w\ve\vl\vc\vo\vm\ve";//"\v for diagonaly,\a for number for beep sound"
+//     printf("%s",str);
+//     return 0;
+// }
+
+// 3 methods to add bool;
+#include <stdio.h>
+// #include <stdbool.h>
+
+// #define bool int
+// #define false 0
+// #define true 1
+
+// typedef enum{false,true}bool;
+
+// int main(){
+//     bool a=true;
+//     bool b=false;
+//     printf("True:%d\n",a);
+//     printf("False:%d\n",b);
+//     return 0;
+// }
+
+
